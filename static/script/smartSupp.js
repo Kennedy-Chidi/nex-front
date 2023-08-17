98dd84738b7a5c60c32da8b90894d355baec876e
Kennedy-Chidi <kennedychidi3@gmail.com>
2023-08-17 15:24:32 +0100

    gggod

--- static/script/smartSupp.js
+++ static/script/smartSupp.js
@@ -1,5 +1,35 @@
+// var _smartsupp = _smartsupp || {};
+// _smartsupp.key = "ccbec4e6b7ca5fbc952a1bd892e04f82c7959337";
+// window.smartsupp ||
+//   (function (d) {
+//     var s,
+//       c,
+//       o = (smartsupp = function () {
+//         o._.push(arguments);
+//       });
+//     o._ = [];
+//     s = d.getElementsByTagName("script")[0];
+//     c = d.createElement("script");
+//     c.type = "text/javascript";
+//     c.charset = "utf-8";
+//     c.async = true;
+//     c.src = "https://www.smartsuppchat.com/loader.js?";
+//     s.parentNode.insertBefore(c, s);
+//   })(document);
+
+// var Tawk_API = Tawk_API || {},
+//   Tawk_LoadStart = new Date();
+// (function () {
+//   var s1 = document.createElement("script"),
+//     s0 = document.getElementsByTagName("script")[0];
+//   s1.async = true;
+//   s1.src = "https://embed.tawk.to/64de21a1cc26a871b02fd07c/1h81r6sne";
+//   s1.charset = "UTF-8";
+//   s1.setAttribute("crossorigin", "*");
+//   s0.parentNode.insertBefore(s1, s0);
+// })();
 var _smartsupp = _smartsupp || {};
-_smartsupp.key = "ccbec4e6b7ca5fbc952a1bd892e04f82c7959337";
+_smartsupp.key = "9d244d23e0440708c8c5bb3e1f9f9f2aa35e1af0";
 window.smartsupp ||
   (function (d) {
     var s,
@@ -16,15 +46,3 @@ window.smartsupp ||
     c.src = "https://www.smartsuppchat.com/loader.js?";
     s.parentNode.insertBefore(c, s);
   })(document);
-
-var Tawk_API = Tawk_API || {},
-  Tawk_LoadStart = new Date();
-(function () {
-  var s1 = document.createElement("script"),
-    s0 = document.getElementsByTagName("script")[0];
-  s1.async = true;
-  s1.src = "https://embed.tawk.to/64de21a1cc26a871b02fd07c/1h81r6sne";
-  s1.charset = "UTF-8";
-  s1.setAttribute("crossorigin", "*");
-  s0.parentNode.insertBefore(s1, s0);
-})();
